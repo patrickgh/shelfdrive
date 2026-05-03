c24c5e0841b22ee6122ce54fa1ada188ba58b383
patrickgh <patrickgh@users.noreply.github.com>
2026-05-03 16:20:48 +0200

    Hide chapter queue from AAOS host

--- app/src/main/java/io/audiobookshelf/aaos/media3/ShelfDriveSessionPolicy.kt
+++ app/src/main/java/io/audiobookshelf/aaos/media3/ShelfDriveSessionPolicy.kt
@@ -72,6 +72,7 @@ internal class ShelfDriveSessionPolicy(
             .remove(Player.COMMAND_SEEK_TO_PREVIOUS_MEDIA_ITEM)
             .remove(Player.COMMAND_SEEK_TO_NEXT)
             .remove(Player.COMMAND_SEEK_TO_PREVIOUS)
+            .remove(Player.COMMAND_GET_TIMELINE)
             .build()
     }
 
